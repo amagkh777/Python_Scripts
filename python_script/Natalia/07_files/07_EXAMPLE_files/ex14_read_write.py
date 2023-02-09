8de646e62e94908f2379a27e69b477f15269f08a
amagkh777 <83203136+amagkh777@users.noreply.github.com>
2023-02-09 15:33:16 +0400

    Delete python_script directory

--- python_script/Natalia/07_files/07_EXAMPLE_files/ex14_read_write.py
+++
@@ -1,7 +0,0 @@
-with open("sh_ip_int_br.txt") as src:
-    with open("result.txt", "w") as dest:
-        for line in src:
-            if line.startswith("Fast"):
-                dest.write(line)
-
-print()
--- python_script/Natalia/12_useful_modules/11_EXAMPLE_modules/ex06_script.py
+++
@@ -1,5 +0,0 @@
-import ex02_use_check_passwd
-from ex01_check_passwd import check_passwd
-import sys
-from pprint import pprint
-
